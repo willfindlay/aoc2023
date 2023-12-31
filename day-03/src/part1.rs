162e78ab7802b991d1f9c814e53352327a9a7e4c
William Findlay <will@isovalent.com>
2023-12-31 16:59:43 -0500

    day 3

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,5 +1,39 @@
-pub fn run(_input: &str) -> anyhow::Result<String> {
-    todo!("part 1 unimplemented")
+pub fn run(input: &str) -> anyhow::Result<String> {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut nums: Vec<u32> = Vec::default();
+    for (y, line) in grid.iter().enumerate() {
+        let mut curr_number: String = String::default();
+        let mut attached_to_symbol = false;
+        for (x, &c) in line.iter().enumerate() {
+            if c.is_numeric() {
+                curr_number.push(c);
+                for x1 in (x.saturating_sub(1))..=(x + 1) {
+                    for y1 in (y.saturating_sub(1))..=(y + 1) {
+                        if let Some(line) = grid.get(y1) {
+                            if let Some(c) = line.get(x1) {
+                                if is_symbol(*c) {
+                                    attached_to_symbol = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if x != line.len() - 1 {
+                    continue;
+                }
+            }
+            if !curr_number.is_empty() && attached_to_symbol {
+                nums.push(curr_number.parse().unwrap())
+            }
+            attached_to_symbol = false;
+            curr_number.clear();
+        }
+    }
+    Ok(nums.iter().sum::<u32>().to_string())
+}
+
+fn is_symbol(c: char) -> bool {
+    !c.is_numeric() && c != '.'
 }
 
 #[cfg(test)]
@@ -8,8 +42,17 @@ mod tests {
 
     #[test]
     fn test_run() -> anyhow::Result<()> {
-        let input = "";
-        assert_eq!("", run(input)?);
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!("4361", run(input)?);
 
         Ok(())
     }
--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,5 +1,51 @@
-pub fn run(_input: &str) -> anyhow::Result<String> {
-    todo!("part 2 unimplemented")
+use std::collections::{BTreeMap, HashSet};
+
+pub fn run(input: &str) -> anyhow::Result<String> {
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let mut maybe_gears: BTreeMap<(usize, usize), Vec<u32>> = Default::default();
+    for (y, line) in grid.iter().enumerate() {
+        let mut curr_number: String = String::default();
+        let mut curr_gears: HashSet<(usize, usize)> = Default::default();
+
+        for (x, &c) in line.iter().enumerate() {
+            if c.is_numeric() {
+                curr_number.push(c);
+                for x1 in (x.saturating_sub(1))..=(x + 1) {
+                    for y1 in (y.saturating_sub(1))..=(y + 1) {
+                        if let Some(line) = grid.get(y1) {
+                            if let Some(c) = line.get(x1) {
+                                if *c == '*' {
+                                    curr_gears.insert((x1, y1));
+                                }
+                            }
+                        }
+                    }
+                }
+                if x != line.len() - 1 {
+                    continue;
+                }
+            }
+
+            if !curr_number.is_empty() {
+                let num: u32 = curr_number.parse().unwrap();
+                for (x, y) in &curr_gears {
+                    maybe_gears
+                        .entry((*x, *y))
+                        .and_modify(|v| v.push(num))
+                        .or_insert(vec![num]);
+                }
+            }
+
+            curr_gears.clear();
+            curr_number.clear();
+        }
+    }
+    Ok(maybe_gears
+        .iter()
+        .filter(|(_, ns)| ns.len() == 2)
+        .map(|(_, ns)| ns.iter().product::<u32>())
+        .sum::<u32>()
+        .to_string())
 }
 
 #[cfg(test)]
@@ -8,8 +54,17 @@ mod tests {
 
     #[test]
     fn test_run() -> anyhow::Result<()> {
-        let input = "";
-        assert_eq!("", run(input)?);
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!("467835", run(input)?);
 
         Ok(())
     }
